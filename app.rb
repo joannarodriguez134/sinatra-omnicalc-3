71f15bcb7d01694df8a676f6a474b8281d226e15
Joanna Rodriguez <140280268+joannarodriguez134@users.noreply.github.com>
2024-02-01 05:26:33 +0000

    Lat/lng working

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require "sinatra"
 require "sinatra/reloader"
+require "http"
 
 get("/") do
   "
@@ -13,5 +14,21 @@ get("/umbrella") do
 end
 
 get ("/process_umbrella") do
+  @user_location = params.fetch("user_loc")
+
+  gmaps_url = "https://maps.googleapis.com/maps/api/geocode/json?address=Merchandise%20Mart%20Chicago&key=" + ENV.fetch("GMAPS_KEY")
+  @raw_response = HTTP.get(gmaps_url).to_s
+  # gmaps_url calls the entire response
+  # to_s gets only the body of the response
+  @parsed_response = JSON.parse(@raw_response)
+
+  @loc_hash = @parsed_response.dig("results", 0, "geometry", "location")
+
+  # dig is a shortcut method used to dig through JSON (instead of using .fetch and .at)
+  # use strings for hashes & use numbers for arrays
+
+  @latitude = @loc_hash.fetch("lat")
+  @longitude = @loc_hash.fetch("lng")
+
   erb(:umbrella_results)
 end
